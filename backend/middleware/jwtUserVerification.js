590b9bde3f73b703bcefeb3a0483f2cf9df627ab
raheem-zk <muhammedraheem144@gmail.com>
2023-09-26 21:53:09 +0530

    Implement User JWT Token Authentication

---
+++ backend/middleware/jwtUserVerification.js
@@ -0,0 +1,29 @@
+import jwt from 'jsonwebtoken';
+
+
+const VerifyToken = (req, res, next)=>{
+    try {
+        const token = req.headers.authorization;
+        if(!token){
+            return res
+            .status(401)
+            .json({message:'Authentication token is missing or invalid.'});
+        }
+
+        const tokenWithoutBearer = token.replace('Bearer ', '');
+        jwt.verify(tokenWithoutBearer, process.env.JWT_SECRET, (err, decoded) => {
+            if (err) {
+              return res.status(401).json({ message: 'Unauthorized' });
+            }
+
+            if(!decoded.role ==='user'){
+                return res.status(401).json({ message: 'Unauthorized' });
+            }
+            next();
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export default VerifyToken;
