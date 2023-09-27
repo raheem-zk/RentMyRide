da6ad10f39208a33050197a9f04dacdfd67b8f10
raheem-zk <muhammedraheem144@gmail.com>
2023-09-27 12:16:16 +0530

    Implement user login and logout with Redux

---
+++ frontend/src/redux/user/authSlice.tsx
@@ -0,0 +1,35 @@
+import { PayloadAction, createSlice } from '@reduxjs/toolkit'
+
+interface UserState {
+    user: UserData | null;
+    success: boolean;
+  }
+  
+  interface UserData {
+    firstName: string;
+    lastName: string;
+    age: number;
+    phoneNumber: number;
+    email: string;
+  }
+const INITTAL_STATE :UserState  = {
+    user: null ,
+    success: false,
+}
+const authSlice = createSlice({
+    name:'user',
+    initialState:INITTAL_STATE,
+    reducers:{
+        userLoggedIn:(state, action : PayloadAction <UserData>)=>{
+            state.user = action.payload;
+            state.success = true;
+        },
+        logout:(state)=>{
+            state.user = null
+            state.success = false;
+        }
+    }
+})
+
+export const { userLoggedIn , logout} = authSlice.actions;
+export default authSlice.reducer;
