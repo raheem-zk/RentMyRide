e3c57a3b2bf445441f56726cb80b0519d49a7195
raheem-zk <muhammedraheem144@gmail.com>
2023-10-07 01:42:54 +0530

    Implemented backend functions for car approval and car rejection

--- backend/controller/admin/carsController.js
+++ backend/controller/admin/carsController.js
@@ -1,13 +1,53 @@
 import carSchema from "../../models/carOwner/car.js";
 
-export const carList = async (req, res)=>{
-    try {
-        console.log('cars list')
-        const data = await carSchema.find() ?? [];
-        console.log(data)
-        return res.json({message:'success',carsData: data})
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ message: 'Internal Server Error', error: true });
+export const carList = async (req, res) => {
+  try {
+    console.log("cars list");
+    const data = (await carSchema.find()) ?? [];
+    console.log(data);
+    return res.json({ message: "success", carsData: data });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ message: "Internal Server Error", error: true });
+  }
+};
+
+export const carApproved = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const result = await carSchema.updateOne(
+      { _id: id },
+      { $set: { status: "Approved" } }
+    );
+    if (result.modifiedCount > 0) {
+      return res.json({ message: "Success" });
     }
-}
+    return res.status(404).json({ message: "Car not found", error: true });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ message: "Internal Server Error", error: true });
+  }
+};
+
+export const carRejected = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const message = req.params.message;
+    const result = await carSchema.updateOne(
+      { _id: id },
+      { $set: { status: "Rejected" } }
+    );
+    if (result.modifiedCount > 0) {
+      return res.json({ message: "Success" });
+    }
+    return res.status(404).json({ message: "Car not found", error: true });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Internal Server Error", error: true });
+  }
+};
--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -2,16 +2,22 @@ import express from "express";
 import { login } from '../controller/admin/AuthController.js'
 import { userList , userBlock, userUnblock } from '../controller/admin/UsersController.js';
 import VerifyToken from "../middleware/jwtAdminVerification.js";
-import { carList } from "../controller/admin/carsController.js";
+import { carApproved, carList, carRejected } from "../controller/admin/carsController.js";
 import { carOwnersList } from "../controller/admin/carOwnersController.js";
 
 const router = express();
 
 router.post('/login',login);
+// users
 router.get('/users',VerifyToken,userList)
 router.patch('/users/:id/unblock',VerifyToken,userUnblock)
 router.patch('/users/:id/block',VerifyToken,userBlock)
 
+// cars
 router.get('/cars',carList)
+router.patch('/cars/:id/approve',carApproved)
+router.patch('/cars/:id/rejected/:message',carRejected)
+
+// car owners
 router.get('/car-owners',carOwnersList)
 export default router;
