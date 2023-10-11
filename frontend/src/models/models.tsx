2dc124d4d4e96b06e792971ca538afb974c60442
raheem-zk <muhammedraheem144@gmail.com>
2023-10-11 20:25:06 +0530

    Update TypeScript Models

--- frontend/src/models/models.tsx
+++ frontend/src/models/models.tsx
@@ -1,50 +1,79 @@
 export interface CarOwnerSignupForm {
-    firstName: string;
-    lastName: string;
-    age: string;
-    phoneNumber: string;
-    email: string;
-    password: string;
-    pincode: string;
-    license: string;
-    place: string;
-    address: string;
-  }
-  
-interface BrandItem {
-  name: string;
-  _id: string;
+  firstName: string;
+  lastName: string;
+  age: string;
+  phoneNumber: string;
+  email: string;
+  password: string;
+  pincode: string;
+  license: string;
+  place: string;
+  address: string;
 }
 
 export interface DropdownProps {
-    data: BrandItem[];
-    title: string;
-    AddingForm: any;
-    HandleForm: (value: any) => void;
-    Reload: ()=> void ;
-    handleCarDetailsChange: (e)=> void;
-  }
+  data: Spec[];
+  title: string;
+  AddingForm: any;
+  HandleForm: (value: any) => void;
+  Reload: () => void;
+  handleCarDetailsChange: (e) => void;
+}
 
-export interface AddingFunction  {
+export interface AddingFunction {
   title: string;
   handleAdding: (value: string) => boolean;
-  Reload: ()=> void;
-  action:any
+  Reload: () => void;
+  action: any;
 }
 
 export interface CarDetailsModel {
-  ownerId: any | undefined;
+  _id: string;
+  ownerId: UserData | undefined | null;
   carName: string;
-  brand: string;
-  model: string;
+  brand: Spec | string;
+  model: Spec | string;
   year: string;
   licensePlate: string;
-  images: any[] | any;
-  transmission: string;
-  category: string;
-  perDayPrice: any ;
+  images: string[];
+  transmission: Spec | string;
+  category: Spec | string;
+  perDayPrice: any;
   description: string;
-  fuelType: string;
+  fuelType: Spec | string;
   startDate: string;
   endDate: string;
+}
+
+interface Spec {
+  _id: string;
+  name: string;
+}
+export interface UserData {
+  _id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber: number | null;
+  place: string | null;
+  age: number | null;
+  address: string | null;
+  license: string | null;
+  profilePicture: string | null;
+  status: boolean;
+}
+
+export interface carOwner {
+  _id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber: number | null;
+  place: string | null;
+  age: number | null;
+  address: string | null;
+  license: string | null;
+  profilePicture: string | null;
+  status: boolean;
+  carId: CarDetailsModel[] | string;
 }
