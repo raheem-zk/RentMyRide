65b3be653974423912e9e9da747475da5efc8167
raheem-zk <muhammedraheem144@gmail.com>
2023-10-14 20:52:12 +0530

    Implemented Car More Details Page in Admin Side

---
+++ frontend/src/page/admin/carMoreDetails.tsx
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+import MoreDetails from "../../component/adminComponent/carMoreDetails";
+
+const CarMoreDetails = () => {
+  return <MoreDetails />;
+};
+
+export default CarMoreDetails;
