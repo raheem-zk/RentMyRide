e5416f276a45726c4bfddd547b14826ae9daba43
raheem-zk <muhammedraheem144@gmail.com>
2023-10-17 19:47:21 +0530

    Implemented car owner status checking on user home page, and added owner blocking/unblocking functionality on the admin controller. Also updated car model to include owner status.

--- backend/controller/user/HomeController.js
+++ backend/controller/user/HomeController.js
@@ -1,18 +1,18 @@
-import carsSchema from '../../models/carOwner/car.js';
-const APPROVEL = 'Approved';
+import carsSchema from "../../models/carOwner/car.js";
+const APPROVEL = "Approved";
 
-export const home = async (req, res)=>{
-    try {
-        console.log(req.headers.authorization)
-        const carsData = await carsSchema.find({status:APPROVEL})
-        .populate('fuelType') 
-        .populate('transmission')
-        .populate('brand')
-        .populate('model')
-        .populate('category')
-        return res.json({message:'success', carsData});
-    } catch (error) {
-        console.log(error);
-    }
-}
-
+export const home = async (req, res) => {
+  try {
+    console.log(req.headers.authorization);
+    const carsData = await carsSchema
+      .find({ status: APPROVEL, ownerStatus: true })
+      .populate("fuelType")
+      .populate("transmission")
+      .populate("brand")
+      .populate("model")
+      .populate("category");
+    return res.json({ message: "success", carsData });
+  } catch (error) {
+    console.log(error);
+  }
+};
