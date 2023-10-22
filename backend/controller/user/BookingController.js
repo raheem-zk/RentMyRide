a195dba6ca83abf7552681407ee41ef459e502f2
raheem-zk <muhammedraheem144@gmail.com>
2023-10-22 21:07:30 +0530

    Added user address and license update functionality in the backend rent booking API

--- backend/controller/user/BookingController.js
+++ backend/controller/user/BookingController.js
@@ -1,13 +1,25 @@
-import OrderShema from '../../models/order.js'; 
+import OrderShema from "../../models/order.js";
+import userSchema from "../../models/user.js";
 
 export const rentBooking = async (req, res) => {
-    try {
-        const data = req.body;
-        const orderModel = new OrderShema(data); 
-        await orderModel.save(); 
-        return res.json({ message: "success" });
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ message: "Internal server error", error: true });
-    }
-}
+  try {
+    const data = req.body;
+    const result = await userSchema.updateOne(
+      { _id: data.userId },
+      {
+        $set: {
+          license: data.license,
+          address: data.address,
+        },
+      }
+    );
+    const orderModel = new OrderShema(data);
+    await orderModel.save();
+    return res.json({ message: "success" });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ message: "Internal server error", error: true });
+  }
+};
