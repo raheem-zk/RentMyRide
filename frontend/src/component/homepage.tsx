0dde6f1bba32bb483197dce23f2807be1c7f7575
raheem-zk <muhammedraheem144@gmail.com>
2023-10-24 09:27:04 +0530

    Refactor home page car card for improved reusability and maintainability.

--- frontend/src/component/homepage.tsx
+++ frontend/src/component/homepage.tsx
@@ -1,62 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CarCard from "./carCard";
+import { CarouselCustomNavigation } from "./homeCarousel";
+import { getHomeCardIteams } from "../api/userApi";
+import { useDispatch } from "react-redux";
+import { addCarsData } from "../redux/user/carsSlice";
 
 function Homepage() {
+  const [data, setData] = useState();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    getCardData();
+  }, []);
+
+  const getCardData = async () => {
+    const response = await getHomeCardIteams();
+    dispatch(addCarsData(response));
+    setData(response);
+  };
+
   return (
-    <>
-      <CarCard />
-
-{/* <div className="bg-gray-300 flex justify-center flex-wrap">
-  <div className="m-2 sm:w-1/1 md:w-1/3 xl:w-1/4">
-    <div className="bg-yellow-300 rounded overflow-hidden h-52">
-      <img className="w-fit h-auto" src={'https://miro.medium.com/v2/resize:fit:434/1*iigDGiNFBOUVJQ_07C1B2g.png'} alt="" />
-
-    </div>
-    <div>
-      <h3>Heading</h3>
-    </div>
-    <div>
-      <p>Category</p>
-    </div>
-  </div>
-  <div className="m-2 sm:w-1/1 md:w-1/3 xl:w-1/4">
-    <div className="bg-yellow-300 rounded overflow-hidden h-52">
-      <img className="w-full h-auto" src={img} alt="" />
-
-    </div>
-    <div>
-      <h3>Heading</h3>
-    </div>
-    <div>
-      <p>Category</p>
-    </div>
-  </div>
-  <div className="m-2 sm:w-1/1 md:w-1/3 xl:w-1/4">
-    <div className="bg-yellow-300 rounded overflow-hidden">
-      <img className="w-full h-auto" src={img} alt="" />
-
-    </div>
-    <div>
-      <h3>Heading</h3>
-    </div>
-    <div>
-      <p>Category</p>
-    </div>
-  </div>
-  <div className="m-2 sm:w-1/1 md:w-1/3 xl:w-1/4">
-    <div className="bg-yellow-300 rounded overflow-hidden">
-      <img className="w-full h-auto" src={img} alt="" />
-
-    </div>
-    <div>
-      <h3>Heading</h3>
-    </div>
-    <div>
-      <p>Category</p>
-    </div>
-  </div>
-</div> */}
-    </>
+    data && (
+      <>
+        <CarCard data={data} />
+      </>
+    )
   );
 }
 
--- frontend/src/page/user/cars.tsx
+++ frontend/src/page/user/cars.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import CarListingPage from '../../component/carsList'
+import React from "react";
+import CarListingPage from "../../component/carsList";
+import { useSelector } from "react-redux";
+import CarCard from "../../component/carCard";
 
 const CarList = () => {
-  return (
-    <CarListingPage/>
-  )
-}
+  const data = useSelector((state: any) => state.carsDatas);
+  return data && (
+    <>
+      <div className="md:block hidden">
+        <CarListingPage data={data.cars} />
+      </div>
+      <div className="md:hidden block">
+        <CarCard data={data.cars} />
+      </div>
+    </>
+  );
+};
 
-export default CarList
+export default CarList;
