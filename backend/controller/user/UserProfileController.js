2223cadb524631e9f37be121e13c59b391ec512b
raheem-zk <muhammedraheem144@gmail.com>
2023-10-25 21:03:31 +0530

    Implement user profile update function in the backend

--- backend/controller/user/UserProfileController.js
+++ backend/controller/user/UserProfileController.js
@@ -1,25 +1,46 @@
 import userSchema from "../../models/user.js";
 import bcrypt from "bcrypt";
 
-export const updatePassword = async (req, res)=>{
-    try {
-        const { password, email } = req.body;
-        const saltRounds = parseInt(process.env.SALTROUNDS);
-    
-        if (!password || !email) {
-          return res.status(400).json({ message: 'Both email and password are required.' });
-        }
-    
-        const hashedPassword = await bcrypt.hash(password, saltRounds);
-        const result = await userSchema.updateOne({ email: email }, { $set: { password: hashedPassword } });
-    
-        if (result.nModified === 0) {
-          return res.status(404).json({ message: 'Email not found or password already set to the new value.' });
-        }
-        
-        return res.json({ message: 'Password updated successfully.' });
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ message: 'Internal Server Error' });
+export const updatePassword = async (req, res) => {
+  try {
+    const { password, email } = req.body;
+    const saltRounds = parseInt(process.env.SALTROUNDS);
+
+    if (!password || !email) {
+      return res
+        .status(400)
+        .json({ message: "Both email and password are required." });
     }
-}
+
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const result = await userSchema.updateOne(
+      { email: email },
+      { $set: { password: hashedPassword } }
+    );
+
+    if (result.nModified === 0) {
+      return res.status(404).json({
+        message: "Email not found or password already set to the new value.",
+      });
+    }
+
+    return res.json({ message: "Password updated successfully." });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const updateProfile = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const data = req.body;
+
+    await userSchema.updateOne({ _id: userId }, data);
+
+    return res.json({ message: "success" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,23 +1,38 @@
 import express from "express";
-import { login, signup, googleSignin, otpVerification, verifySignup, verifyForgot } from "../controller/user/AuthController.js";
-import { home } from '../controller/user/HomeController.js'; 
-import VerifyToken from '../middleware/jwtUserVerification.js';
-import { bookingCheckoutSession, rentBooking } from "../controller/user/BookingController.js";
-import { updatePassword } from "../controller/user/UserProfileController.js";
+import {
+  login,
+  signup,
+  googleSignin,
+  otpVerification,
+  verifySignup,
+  verifyForgot,
+} from "../controller/user/AuthController.js";
+import { home } from "../controller/user/HomeController.js";
+import VerifyToken from "../middleware/jwtUserVerification.js";
+import {
+  bookingCheckoutSession,
+  rentBooking,
+} from "../controller/user/BookingController.js";
+import {
+  updatePassword,
+  updateProfile,
+} from "../controller/user/UserProfileController.js";
 
 const router = express();
 
-router.post('/login',login);
-router.post('/signup',signup)
-router.post('/signup-verify',verifySignup)
-router.post('/otp-verification',otpVerification)
-router.post('/google-signin',googleSignin)
-router.get('/',home);
+router.post("/login", login);
+router.post("/signup", signup);
+router.post("/signup-verify", verifySignup);
+router.post("/otp-verification", otpVerification);
+router.post("/google-signin", googleSignin);
+router.get("/", home);
 
-router.post('/forgot-password',verifyForgot)
-router.post('/forgot-password/otp',otpVerification)
-router.post('/forgot-password/set-password',updatePassword)
+router.post("/forgot-password", verifyForgot);
+router.post("/forgot-password/otp", otpVerification);
+router.post("/forgot-password/set-password", updatePassword);
 
-router.post('/rent-booking/confirm',VerifyToken, rentBooking)
-router.post('/rent-booking',bookingCheckoutSession);
+router.post("/rent-booking/confirm", VerifyToken, rentBooking);
+router.post("/rent-booking", VerifyToken, bookingCheckoutSession);
+
+router.patch("/profile/:userId/edit", VerifyToken, updateProfile);
 export default router;
