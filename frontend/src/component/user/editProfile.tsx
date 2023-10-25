1a6a64e8200e5247c2a9b2fc160baa04135b1dd2
raheem-zk <muhammedraheem144@gmail.com>
2023-10-25 21:29:12 +0530

    Improved Edit Profile Page Styling for Responsiveness

--- frontend/src/component/user/editProfile.tsx
+++ frontend/src/component/user/editProfile.tsx
@@ -84,42 +84,51 @@ const EditProfile = ({ data }) => {
     <div className="max-w-screen-lg mx-auto p-4">
       <h1 className="text-2xl font-semibold mb-4">Edit Profile</h1>
       <form onSubmit={handleSubmit}>
-        <div className="grid grid-cols-2 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          {/** Input Fields */}
           <div>
-            <label htmlFor="firstName">First Name</label>
+            <label htmlFor="firstName" className="text-gray-600">
+              First Name
+            </label>
             <input
               type="text"
               id="firstName"
               name="firstName"
               value={user?.firstName}
               onChange={handleInputChange}
-              className="w-full p-2 border rounded"
+              className="w-full p-2 border rounded focus:outline-none focus:ring focus:border-blue-400"
             />
           </div>
           <div>
-            <label htmlFor="lastName">Last Name</label>
+            <label htmlFor="lastName" className="text-gray-600">
+              Last Name
+            </label>
             <input
               type="text"
               id="lastName"
               name="lastName"
               value={user?.lastName}
               onChange={handleInputChange}
-              className="w-full p-2 border rounded"
+              className="w-full p-2 border rounded focus:outline-none focus:ring focus:border-blue-400"
             />
           </div>
           <div>
-            <label htmlFor="email">Email</label>
+            <label htmlFor="email" className="text-gray-600">
+              Email
+            </label>
             <input
               type="email"
               id="email"
               name="email"
               value={user?.email}
               onChange={handleInputChange}
-              className="w-full p-2 border rounded"
+              className="w-full p-2 border rounded focus:outline-none focus:ring focus:border-blue-400"
             />
           </div>
           <div>
-            <label htmlFor="phoneNumber">Phone Number</label>
+            <label htmlFor="phoneNumber" className="text-gray-600">
+              Phone Number
+            </label>
             <input
               type="tel"
               id="phoneNumber"
@@ -128,22 +137,26 @@ const EditProfile = ({ data }) => {
               maxLength={10}
               value={user?.phoneNumber}
               onChange={handleInputChange}
-              className="w-full p-2 border rounded"
+              className="w-full p-2 border rounded focus:outline-none focus:ring focus:border-blue-400"
             />
           </div>
           <div>
-            <label htmlFor="place">Place</label>
+            <label htmlFor="place" className="text-gray-600">
+              Place
+            </label>
             <input
               type="text"
               id="place"
               name="place"
               value={user?.place}
               onChange={handleInputChange}
-              className="w-full p-2 border rounded"
+              className="w-full p-2 border rounded focus:outline-none focus:ring focus:border-blue-400"
             />
           </div>
           <div>
-            <label htmlFor="age">Age</label>
+            <label htmlFor="age" className="text-gray-600">
+              Age
+            </label>
             <input
               type="number"
               id="age"
@@ -152,22 +165,26 @@ const EditProfile = ({ data }) => {
               min={18}
               value={user?.age}
               onChange={handleInputChange}
-              className="w-full p-2 border rounded"
+              className="w-full p-2 border rounded focus:outline-none focus:ring focus:border-blue-400"
             />
           </div>
           <div>
-            <label htmlFor="address">Address</label>
+            <label htmlFor="address" className="text-gray-600">
+              Address
+            </label>
             <input
               type="text"
               id="address"
               name="address"
               value={user?.address}
               onChange={handleInputChange}
-              className="w-full p-2 border rounded"
+              className="w-full p-2 border rounded focus:outline-none focus:ring focus:border-blue-400"
             />
           </div>
           <div>
-            <label htmlFor="license">License</label>
+            <label htmlFor="license" className="text-gray-600">
+              License
+            </label>
             <input
               type="text"
               id="license"
@@ -175,13 +192,14 @@ const EditProfile = ({ data }) => {
               maxLength={16}
               value={user?.license}
               onChange={handleInputChange}
-              className="w-full p-2 border rounded"
+              className="w-full p-2 border rounded focus:outline-none focus:ring focus:border-blue-400"
             />
           </div>
         </div>
+
         <button
           type="submit"
-          className="mt-4 bg-blue-500 text-white p-2 rounded hover:bg-blue-600"
+          className="mt-4 bg-blue-500 text-white p-2 rounded hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-400"
         >
           Save Changes
         </button>
