9990f2a24f906fda7920ea730f16cb88f5292178
raheem-zk <muhammedraheem144@gmail.com>
2023-10-27 15:33:16 +0530

    Refined Order History Page with Dynamic Data

--- frontend/src/component/user/orderCard.tsx
+++ frontend/src/component/user/orderCard.tsx
@@ -4,22 +4,21 @@ const OrderCard = ({ order }) => {
   
   return (
     <div className="bg-white shadow-md rounded p-4">
-      <h2 className="text-xl font-semibold mb-2">Order #{order?.orderNumber}</h2>
-      <div className="text-gray-600 mb-4">Order Date: {order?.orderDate}</div>
+      <h2 className="text-xl font-semibold mb-2">Order #{order?.orderId}</h2>
+      <div className="text-gray-600 mb-4">Order Date: {order?.orderDate ? new Date (order?.orderDate).toLocaleDateString() : ''}</div>
       
       <div className="flex items-center justify-between mb-4">
         <div>
           <p className="font-semibold text-gray-800">Car Details:</p>
-          <p>{order?.carDetails}</p>
+          <p>{order?.carId?.year} ,{order?.carId?.carName} </p>
         </div>
         <div>
           <p className="font-semibold text-gray-800">Status:</p>
-          <p className={`text-${order?.status === 'Completed' ? 'green' : 'red'}-500`}>
+          <p className={`text-${order?.status === 'rejected' ? 'red' : (order?.status === 'pending' ? 'yellow' : 'green')}-500`}>
             {order?.status}
           </p>
         </div>
       </div>
-      
       <div className="flex items-center justify-between">
         <div>
           <p className="font-semibold text-gray-800">Total Price:</p>
@@ -27,7 +26,7 @@ const OrderCard = ({ order }) => {
         </div>
         <div>
           <p className="font-semibold text-gray-800">Pickup Date:</p>
-          <p>{order?.pickupDate}</p>
+          <p>{new Date(order?.pickupDate).toLocaleDateString()}</p>
         </div>
       </div>
     </div>
--- frontend/src/component/user/orderHistory.tsx
+++ frontend/src/component/user/orderHistory.tsx
@@ -2,58 +2,73 @@ import React, { useState, useEffect } from "react";
 import OrderCard from "./orderCard";
 import { useSelector } from "react-redux";
 import { getuserOrders } from "../../api/userApi";
+import { FcFilledFilter } from "react-icons/fc";
+import Pagination from '../pagination';
 
 const OrderHistory = () => {
   const { user } = useSelector((state: any) => state?.userAuth);
   const [orders, setOrders] = useState([]);
-
+  const [filter, setFilter] = useState(false);
+  const [size, setSize] = useState(0);
   useEffect(() => {
     getOrder();
-    const data = [{
-      orderNumber: '12345',
-      orderDate: '2023-10-26',
-      carDetails: '2023 Honda Civic, Blue',
-      status: 'Completed',
-      totalPrice: 5000,
-      pickupDate: '2023-10-30',
-    },{
-      orderNumber: '12345',
-      orderDate: '2023-10-26',
-      carDetails: '2023 Honda Civic, Blue',
-      status: 'Completed',
-      totalPrice: 5000,
-      pickupDate: '2023-10-30',
-    },{
-      orderNumber: '12345',
-      orderDate: '2023-10-26',
-      carDetails: '2023 Honda Civic, Blue',
-      status: 'Completed',
-      totalPrice: 5000,
-      pickupDate: '2023-10-30',
-    },{
-      orderNumber: '12345',
-      orderDate: '2023-10-26',
-      carDetails: '2023 Honda Civic, Blue',
-      status: 'Completed',
-      totalPrice: 5000,
-      pickupDate: '2023-10-30',
-    },]
-    setOrders(data);
   }, []);
 
   const getOrder = async () => {
-    const data = await getuserOrders(user._id);
+    const { ordersData, size } = await getuserOrders(user._id, 1);
+    console.log(ordersData, size);
+    setSize(size);
+    setOrders(ordersData);
+  };
+
+  const handleClick = () => {
+    setFilter(!filter);
+  };
+  const filterPagination = async (pageNumber) => {
+    const { ordersData } = await getuserOrders(user._id, pageNumber);
+    setOrders(ordersData);
   };
   return (
     <div className="max-w-screen-lg mx-auto p-4">
-      <h1 className="text-3xl font-extrabold mb-4">Order History</h1>
-
-      {orders.length > 0 ? (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {orders.map((order) => (
-            <OrderCard key={order?._id} order={order} />
-          ))}
+      <div className=" justify-between flex">
+        <h1 className="text-3xl font-extrabold mb-4">Order History</h1>
+        <div className="relative right-0">
+          <div className="flex items-center">
+            <button
+              className="mr-2 px-1 py-2 text-gray-700"
+              onClick={handleClick}
+            >
+              Filter
+            </button>
+            <FcFilledFilter
+              onClick={handleClick}
+              className="h-5 w-5 text-gray-700 cursor-pointer"
+            />
+          </div>
+          {filter && (
+            <div className="absolute right-0 mt-2 w-40 bg-white rounded-lg shadow-md py-1">
+              <button className="w-full text-left px-3 py-2 block hover:bg-gray-100">
+                Pending
+              </button>
+              <button className="w-full text-left px-3 py-2 block hover:bg-gray-100">
+                Completed
+              </button>
+              <button className="w-full text-left px-3 py-2 block hover:bg-gray-100">
+                Rented
+              </button>
+            </div>
+          )}
         </div>
+      </div>
+      {orders.length > 0 ? (
+        <>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            {orders.map((order) => (
+              <OrderCard key={order?._id} order={order} />
+            ))}
+          </div>
+          <Pagination size={size} filterPagination={filterPagination} />
+        </>
       ) : (
         <p className="text-gray-600">No orders found in your history.</p>
       )}
