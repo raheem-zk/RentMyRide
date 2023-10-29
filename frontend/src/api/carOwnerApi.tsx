a075058fc92de4590979cf94e79f721e093ccaf4
raheem-zk <muhammedraheem144@gmail.com>
2023-10-29 21:21:10 +0530

    Add Cloudinary Image Upload API Integration

--- frontend/src/api/carOwnerApi.tsx
+++ frontend/src/api/carOwnerApi.tsx
@@ -1,14 +1,37 @@
-import { carOwnerAxios } from "../axios/axios"
+import axios from "axios";
+import { carOwnerAxios } from "../axios/axios";
+import { ErrorMessage } from "../utils/utils";
 
-export const getOrdersList = async (ownerId)=>{
-    return await carOwnerAxios.get(`/orders/${ownerId}`);
-}
+export const getOrdersList = async (ownerId) => {
+  return await carOwnerAxios.get(`/orders/${ownerId}`);
+};
 
-export const approveOrder = async (orderId)=>{
-    await carOwnerAxios.patch(`/order/approve/${orderId}`);
-}
+export const approveOrder = async (orderId) => {
+  await carOwnerAxios.patch(`/order/approve/${orderId}`);
+};
 
-export const rejectOrder = async (orderId)=>{
-    await carOwnerAxios.patch(`/order/reject/${orderId}`);
-}
+export const rejectOrder = async (orderId) => {
+  await carOwnerAxios.patch(`/order/reject/${orderId}`);
+};
+
+export const uploadCarImage = async (img) => {
+  const presetKey = import.meta.env.VITE_PRESETKEY;
+  const cloudName = import.meta.env.VITE_CLOUD_NAME;
+
+  const formData = new FormData();
+  formData.append("file", img);
+  formData.append("upload_preset", presetKey);
+  formData.append("cloud_name", cloudName);
+
+  const response = await axios.post(
+    import.meta.env.VITE_CLOUDINERY_API,
+    formData
+  );
+  console.log("test images url", response.data.url);
+  if (response.status === 200) {
+    return response.data.url;
+  } else {
+    return ErrorMessage("Failed to upload the image");
+  }
+};
 
