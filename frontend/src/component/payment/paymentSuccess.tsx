217a549b372edd30b6d046beb0572e454da61768
raheem-zk <muhammedraheem144@gmail.com>
2023-10-29 18:02:01 +0530

    Update user wallet data on payment success

--- frontend/src/component/payment/paymentSuccess.tsx
+++ frontend/src/component/payment/paymentSuccess.tsx
@@ -1,31 +1,47 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { booking } from "../../api/userApi";
+import { booking, updatedUserData } from "../../api/userApi";
 import { clearBooking } from "../../redux/user/bookingSlice";
 import { useNavigate, useParams } from "react-router-dom";
+import Loading from "../loading";
+import { updateData } from "../../redux/user/authSlice";
 
 const PaymentSuccess = () => {
   const { orderId } = useParams();
   const { bookingData } = useSelector((state: any) => state.bookingData);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
+
   const uploadBooking = async () => {
     const response = await booking(bookingData);
     if (response) {
       dispatch(clearBooking());
+      setLoading(false)
+      if(bookingData?.paymentMethod=='wallet'){
+        const updatedData = await updatedUserData(bookingData?.userId);
+        dispatch(updateData(updatedData));
+      }
     }
   };
+  
+  
+useEffect(()=>{
+  if(!orderId || !bookingData){
+    navigate('/');
+    return 
+  }
 
-  useEffect(() => {
-    if (orderId != bookingData?.orderId) {
-      navigate("/");
-    }
-    if (bookingData) {
-      console.log("Booking data here:", bookingData);
-      uploadBooking();
-    }
-  }, [bookingData]);
-  return (
+  if(bookingData && bookingData?.orderId !== orderId){
+    navigate('/');
+    return
+  }
+  if(bookingData?.orderId === orderId){
+    uploadBooking()
+  }
+},[])
+
+  return loading ? <Loading/> : (
     <div className="min-h-screen flex items-center justify-center bg-green-200">
       <div className="bg-white p-8 rounded-lg shadow-lg">
         <h2 className="text-2xl font-bold mb-4">Payment Successful</h2>
