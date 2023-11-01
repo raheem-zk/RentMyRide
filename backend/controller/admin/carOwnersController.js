f5e17e5599c8bad2f58f8d7d7b0e5d6701b1052a
raheem-zk <muhammedraheem144@gmail.com>
2023-11-02 03:10:31 +0530

    Added pagination to retrieve car owners data

--- backend/controller/admin/carOwnersController.js
+++ backend/controller/admin/carOwnersController.js
@@ -1,10 +1,29 @@
 import carOwnerSchema from "../../models/carOwner/carOwner.js";
 import carSchema from "../../models/carOwner/car.js";
 
+const LIMIT = 10;
+
 export const carOwnersList = async (req, res) => {
   try {
-    const carownersData = (await carOwnerSchema.find().populate("carId")) ?? [];
-    res.json({ message: "success", carownersData });
+    const PAGE = req?.query?.page
+      ? req.query.page >= 1
+        ? req.query.page
+        : 1
+      : 1;
+    const SKIP = (PAGE - 1) * LIMIT;
+
+    const carownersData =
+      (await carOwnerSchema
+        .find()
+        .populate("carId")
+        .sort({ _id: -1 })
+        .skip(SKIP)
+        .limit(LIMIT)) ?? [];
+
+    const TotalSize = await carOwnerSchema.countDocuments();
+    const size = Math.ceil(TotalSize / LIMIT);
+
+    return res.json({ message: "success", carownersData, size });
   } catch (error) {
     console.error(error);
     return res
@@ -16,7 +35,6 @@ export const carOwnersList = async (req, res) => {
 export const ownerBlock = async (req, res) => {
   try {
     const id = req.params.id;
-    console.log(id, " its heare");
     let result = await carOwnerSchema.updateOne(
       { _id: id },
       { $set: { status: false } }
