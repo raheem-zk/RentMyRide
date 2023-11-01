216346eade2640ab5cb87197c68dd4fd87585baf
raheem-zk <muhammedraheem144@gmail.com>
2023-11-02 03:11:34 +0530

    Added pagination to retrieve orders data

--- backend/controller/admin/controller.js
+++ backend/controller/admin/controller.js
@@ -1,9 +1,26 @@
 import orderShema from '../../models/order.js';
 
+const LIMIT = 10;
+
 export const getOrders = async (req, res) => {
   try {
-    const data = await orderShema.find().populate('carId').populate('userId').sort({_id:-1}) ?? [];
-    return res.json({message:'success', ordersData: data})
+    const PAGE = req?.query?.page
+      ? req.query.page >= 1
+        ? req.query.page
+        : 1
+      : 1;
+
+    const SKIP = (PAGE - 1) * LIMIT;
+
+    const data = await orderShema.find().populate('carId').populate('userId')
+    .sort({_id:-1})
+    .skip(SKIP)
+    .limit(LIMIT) ?? [];
+
+    const TotalSize = await orderShema.countDocuments();
+    const size = Math.ceil(TotalSize / LIMIT);
+    
+    return res.json({message:'success', ordersData: data, size})
   } catch (error) {
     console.error(error);
     return res
