25cf46df47c2b10967cdfcf67672c9aab799a972
raheem-zk <muhammedraheem144@gmail.com>
2023-11-02 03:03:00 +0530

    Add user data to pagination feature

--- backend/controller/admin/UsersController.js
+++ backend/controller/admin/UsersController.js
@@ -1,41 +1,69 @@
-import userModel from '../../models/user.js';
+import userModel from "../../models/user.js";
 
-export const userList = async (req,res)=>{
-    try {
-        const userData = await userModel.find();
-        res.json({userData})
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ message: 'Internal Server Error', error: true });
-    }
-}
+const LIMIT = 10;
+
+export const userList = async (req, res) => {
+  try {
+    const PAGE = req?.query?.page
+      ? req.query.page >= 1
+        ? req.query.page
+        : 1
+      : 1;
+    const SKIP = (PAGE - 1) * LIMIT;
+
+    const userData = await userModel
+      .find()
+      .sort({ _id: -1 })
+      .skip(SKIP)
+      .limit(LIMIT);
+      
+    const TotalSize = await userModel.countDocuments();
+    const size = Math.ceil(TotalSize / LIMIT);
+
+    return res.json({ message: "success", userData, size });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ message: "Internal Server Error", error: true });
+  }
+};
 
 export const userUnblock = async (req, res) => {
-    try {
-      const id = req.params.id;
-      let result = await userModel.updateOne({ _id: id }, { $set: { status: true } });
+  try {
+    const id = req.params.id;
+    let result = await userModel.updateOne(
+      { _id: id },
+      { $set: { status: true } }
+    );
 
-      if (result.modifiedCount > 0) {
-        return res.json({ message: 'Success' });
-      }
-      return res.status(404).json({ message: 'User not found', error: true });
-    } catch (error) {
-      console.error(error);
-      return res.status(500).json({ message: 'Internal Server Error', error: true });
+    if (result.modifiedCount > 0) {
+      return res.json({ message: "Success" });
     }
-  };
-  
+    return res.status(404).json({ message: "User not found", error: true });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ message: "Internal Server Error", error: true });
+  }
+};
+
 export const userBlock = async (req, res) => {
-    try {
-      const id = req.params.id;
-      let result = await userModel.updateOne({ _id: id }, { $set: { status: false } });
-      if (result.modifiedCount > 0) {
-        return res.json({ message: 'Success' });
-      }
-      return res.status(404).json({ message: 'User not found', error: true });
-    } catch (error) {
-      console.error(error);
-      return res.status(500).json({ message: 'Internal Server Error', error: true });
+  try {
+    const id = req.params.id;
+    let result = await userModel.updateOne(
+      { _id: id },
+      { $set: { status: false } }
+    );
+    if (result.modifiedCount > 0) {
+      return res.json({ message: "Success" });
     }
-  };
-  
+    return res.status(404).json({ message: "User not found", error: true });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ message: "Internal Server Error", error: true });
+  }
+};
