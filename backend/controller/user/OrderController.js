ec9978b8881e5bbd72b1d5fd8502143e89e120a9
raheem-zk <muhammedraheem144@gmail.com>
2023-11-02 12:46:53 +0530

    Added order history side filtering in the backend

--- backend/controller/user/OrderController.js
+++ backend/controller/user/OrderController.js
@@ -14,14 +14,19 @@ export const getUserOrders = async (req, res) => {
     const size = Math.ceil(TotalSize / LIMIT);
     const SKIP = (page - 1) * LIMIT;
 
-    const ordersData = await orderSchema
-      .find({ userId: userId, paymentStatus: "Paid" })
+    let query = { userId: userId, paymentStatus: "Paid" };
+    if (req.query.filterValue) {
+      query.status = req.query.filterValue;
+    }
+      const ordersData = await orderSchema
+      .find(query)
       .populate("carId")
       .sort({ _id: -1 })
       .skip(SKIP)
       .limit(LIMIT);
 
     return res.json({ message: "success", ordersData, size });
+    
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: "Internal Server Error" });
