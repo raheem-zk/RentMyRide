0f6a46db77ff87874c073796c522b1974c094fd8
raheem-zk <muhammedraheem144@gmail.com>
2023-11-06 21:30:49 +0530

    Updated order cancel handling for improved functionality

--- frontend/src/component/carOwner/orderActionDropdown.tsx
+++ frontend/src/component/carOwner/orderActionDropdown.tsx
@@ -3,7 +3,7 @@ import { AiOutlineCheck, AiOutlineClose } from "react-icons/ai";
 import { CiCircleMore } from "react-icons/ci";
 import { approveOrder, rejectOrder } from "../../api/carOwnerApi";
 
-function ActionDropdown({ orderId, reload }:any) {
+function ActionDropdown({ orderId, reload, cancel }: any) {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleDropdown = () => {
@@ -33,7 +33,6 @@ function ActionDropdown({ orderId, reload }:any) {
       >
         <CiCircleMore size={20} />
       </button>
-
       <div
         className={`${
           isOpen ? "block" : "hidden"
@@ -43,24 +42,37 @@ function ActionDropdown({ orderId, reload }:any) {
         aria-labelledby="options-menu-button"
       >
         <div className="py-1 w-full">
-          <div className="py-1 w-full">
-            <button
-              onClick={onApprove}
-              className="px-4 py-2 text-sm text-indigo-600 hover:bg-indigo-50 hover:text-indigo-700 flex items-center focus:outline-none"
-            >
-              <span className="mr-2">Approve</span>
-              <AiOutlineCheck className="text-green-500" />
-            </button>
-          </div>
-          <div className="py-1 w-full">
-            <button
-              onClick={onReject}
-              className="px-4 py-2 text-sm text-red-600 hover:bg-red-50 hover:text-red-700 flex items-center focus:outline-none"
-            >
-              <span className="mr-2">Reject</span>
-              <AiOutlineClose className="text-red-500" />
-            </button>
-          </div>
+          {cancel ? (
+            <div className="py-1 w-full">
+              <button
+                onClick={onReject}
+                className="px-4 py-2 text-sm text-red-600 hover:bg-red-100 hover:text-red-800 flex items-center focus:outline-none rounded-md border border-red-500"
+              >
+                <span className="mr-2">Approve Cancel Request</span>
+              </button>
+            </div>
+          ) : (
+            <>
+              <div className="py-1 w-full">
+                <button
+                  onClick={onApprove}
+                  className="px-4 py-2 text-sm text-indigo-600 hover:bg-indigo-50 hover:text-indigo-700 flex items-center focus:outline-none"
+                >
+                  <span className="mr-2">Approve</span>
+                  <AiOutlineCheck className="text-green-500" />
+                </button>
+              </div>
+              <div className="py-1 w-full">
+                <button
+                  onClick={onReject}
+                  className="px-4 py-2 text-sm text-red-600 hover:bg-red-50 hover:text-red-700 flex items-center focus:outline-none"
+                >
+                  <span className="mr-2">Reject</span>
+                  <AiOutlineClose className="text-red-500" />
+                </button>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
--- frontend/src/component/carOwner/orederMoreDetails.tsx
+++ frontend/src/component/carOwner/orederMoreDetails.tsx
@@ -1,102 +1,131 @@
 import React from "react";
-import { NotUpdated } from "../../utils/adminUtils";
-import { Link } from "react-router-dom";
+
+import Loading from "../loading";
 
 const MoreDetails = ({ order, car, user }) => {
-  return order && (
-    <div className="md:m-5 bg-white p-4 rounded-lg shadow-lg">
+  return !order ? (
+    <Loading />
+  ) : (
+    <div className="bg-gray-100 p-4 rounded-lg">
       <h1 className="text-3xl font-semibold text-center mb-4 text-indigo-600">
         Order Details
       </h1>
-      <table className="w-full">
-        <tr>
-          <th className="bg-gray-800 text-white p-2">Car Name</th>
-          <td className="p-2">{car?.carName}</td>
-        </tr>
-        <tr>
-            <th className="bg-gray-800 text-white p-2">Car Photo</th>
-            <td className="p-2">
-                {car?.images ? (
-                    <Link to={`/car-owner/car-details/${car?._id}`}><img
-                        src={car.images[0]}
-                        alt="Profile"
-                        className="rounded-full w-16 h-16"
-                    />
-                    </Link>
-                ) : <NotUpdated />}
-            </td>
-        </tr>
-        <tr>
-          <th className="bg-gray-800 text-white p-2">Car Model</th>
-          <td className="p-2">{car?.model?.name}</td>
-        </tr>
-        <tr>
-            <th className="bg-gray-800 text-white p-2">User Profile</th>
-            <td className="p-2">
-                {user?.profile ? (
-                    <img
-                        src={user.profile}
-                        alt="Profile"
-                        className="rounded-full w-16 h-16"
-                    />
-                ) : <NotUpdated />}
-            </td>
-        </tr>
-        <tr>
-          <th className="bg-gray-800 text-white p-2">User Full Name</th>
-          <td className="p-2">{user?.firstName} {user?.lastName}</td>
-        </tr>
-        <tr>
-          <th className="bg-gray-800 text-white p-2">Pickup Date</th>
-          <td className="p-2">{order?.pickupDate? new Date(order?.dropoffDate).toLocaleDateString() : ''}</td>
-        </tr>
-        <tr>
-          <th className="bg-gray-800 text-white p-2">Dropoff Date</th>
-          <td className="p-2">{order?.dropoffDate ? new Date(order?.dropoffDate).toLocaleDateString() : ''}</td>
-        </tr>
-        <tr>
-          <th className="bg-gray-800 text-white p-2">Pickup Location</th>
-          <td className="p-2">{order?.pickupLocation }</td>
-        </tr>
-        <tr>
-          <th className="bg-gray-800 text-white p-2">Pickup Time</th>
-          <td className="p-2">{order?.pickupTime }</td>
-        </tr>
-        <tr>
-          <th className="bg-gray-800 text-white p-2">Dropoff Time</th>
-          <td className="p-2">{order?.dropoffTime }</td>
-        </tr>
-        <tr>
-          <th className="bg-gray-800 text-white p-2">Total Price</th>
-          <td className="p-2">{order?.totalPrice }</td>
-        </tr>
-        <tr>
-          <th className="bg-gray-800 text-white p-2">Status</th>
-          <td className={`p-2 ${order?.status === "Approved" ? "text-green-500" : "text-red-500"}`}>
-            {order?.status}
-          </td>
-        </tr>
-        <tr>
-          <th className="bg-gray-800 text-white p-2">Car License Plate</th>
-          <td className="p-2">{car?.licensePlate}</td>
-        </tr>
-        <tr>
-          <th className="bg-gray-800 text-white p-2">User Email</th>
-          <td className="p-2 text-blue-500">{user?.email}</td>
-        </tr>
-        <tr>
-          <th className="bg-gray-800 text-white p-2">User Phone Number</th>
-          <td className="p-2 text-blue-500">{user?.phoneNumber}</td>
-        </tr>
-        <tr>
-          <th className="bg-gray-800 text-white p-2">License</th>
-          <td className="p-2 text-blue-500">{user?.license}</td>
-        </tr>
-        <tr>
-          <th className="bg-gray-800 text-white p-2">Address</th>
-          <td className="p-2 text-blue-500">{user?.address}</td>
-        </tr>
-      </table>
+      <div className="grid grid-cols-2 gap-4">
+        <div>
+          <p className="text-sm">
+            Order ID: <span className="font-semibold">{order.orderId}</span>
+          </p>
+          <p className="text-sm">
+            Payment Method:{" "}
+            <span className="font-semibold">{order.paymentMethod}</span>
+          </p>
+          <p className="text-sm">
+            Pickup Date:{" "}
+            <span className="font-semibold">{order.pickupDate}</span>
+          </p>
+          <p className="text-sm">
+            Dropoff Date:{" "}
+            <span className="font-semibold">{order.dropoffDate}</span>
+          </p>
+          <p className="text-sm">
+            Pickup Time:{" "}
+            <span className="font-semibold">{order.pickupTime}</span>
+          </p>
+          <p className="text-sm">
+            Dropoff Time:{" "}
+            <span className="font-semibold">{order.dropoffTime}</span>
+          </p>
+        </div>
+        <div>
+          <p className="text-sm">
+            Payment Method:{" "}
+            <span className="font-semibold">{order.paymentMethod}</span>
+          </p>
+          <p className="text-sm">
+            Phone Number: <span className="font-semibold">{order.phone}</span>
+          </p>
+          <p className="text-sm">
+            Pickup Location:{" "}
+            <span className="font-semibold">{order.pickupLocation}</span>
+          </p>
+          <p className="text-sm">
+            Dropoff Location:{" "}
+            <span className="font-semibold">{order.dropoffLocation}</span>
+          </p>
+          <p className="text-sm">
+            Email: <span className="font-semibold">{order.email}</span>
+          </p>
+          <p className="text-sm">
+            License: <span className="font-semibold">{order.license}</span>
+          </p>
+          <p className="text-sm">
+            Total Days: <span className="font-semibold">{order.totalDays}</span>
+          </p>
+          <p className="text-sm">
+            Total Price:{" "}
+            <span className="font-semibold">{order.totalPrice}</span>
+          </p>
+          <p className="text-sm">
+            Per Day Price:{" "}
+            <span className="font-semibold">{order.perDayPrice}</span>
+          </p>
+          <p className="text-sm">
+            Status: <span className="font-semibold">{order.status}</span>
+          </p>
+          {
+            order?.cancellationReason && (
+              <p className="text-sm">
+              Cancellation Reason: <span className="font-semibold">{order.cancellationReason}</span>
+            </p>
+            )
+          }
+          <p className="text-sm">
+            Payment Status:{" "}
+            <span className="font-semibold">{order.paymentStatus}</span>
+          </p>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-4">
+        <div className="flex justify-center items-center">
+          <img
+            src={
+              user?.profilePicture
+                ? user.profilePicture
+                : "https://thumbs.dreamstime.com/b/flat-male-avatar-image-beard-hairstyle-businessman-profile-icon-vector-179285629.jpg"
+            }
+            alt="User"
+            className="w-32 h-32 rounded-full"
+          />
+        </div>
+        <div className="flex-col justify-center my-5">
+          <p className="text-lg mb-2">
+            Name:{" "}
+            <span className="font-semibold">
+              {user?.firstName} {user?.lastName}
+            </span>
+          </p>
+          <p className="text-lg mb-2">
+            Email: <span className="font-semibold">{user?.email}</span>
+          </p>
+          <p className="text-lg mb-2">
+            Phone Number:{" "}
+            <span className="font-semibold">{user?.phoneNumber}</span>
+          </p>
+          <p className="text-lg mb-2">
+            License Number:{" "}
+            <span className="font-semibold">{user?.license}</span>
+          </p>
+          <p className="text-lg mb-2">
+            Address: <span className="font-semibold">{user?.address}</span>
+          </p>
+          <p className="text-lg mb-2">
+            Age:{" "}
+            <span className="font-semibold">
+              {user?.age ? user?.age : "not Updated"}
+            </span>
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
