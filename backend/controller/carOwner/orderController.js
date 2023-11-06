afeca87272ca3ee01341799142cb9c3cbbdf5633
raheem-zk <muhammedraheem144@gmail.com>
2023-11-06 12:51:46 +0530

    Implement payment return and Add order page pagination

--- backend/controller/carOwner/orderController.js
+++ backend/controller/carOwner/orderController.js
@@ -1,24 +1,39 @@
 import ordersSchema from "../../models/order.js";
 import carSchema from "../../models/carOwner/car.js";
+import walletSchema from "../../models/wallet.js";
+
 const APPROVE = "approved";
 const REJECT = "rejected";
+const LIMIT = 3;
+const PAID = "Paid";
 
 export const orders = async (req, res) => {
   try {
-    const { ownerId } = req.params;
+    const { ownerId, page } = req.params;
 
     const cars = await carSchema.find({ ownerId });
     const carIds = cars.map((car) => car._id);
 
+    const TotalSize = await carSchema.countDocuments({
+      ownerId,
+      paymentStatus: PAID,
+    });
+
+    const size = Math.ceil(TotalSize / LIMIT);
+    const SKIP = (page - 1) * LIMIT;
+
     const ordersData = await ordersSchema
       .find({
         carId: { $in: carIds },
+        paymentStatus: PAID,
       })
       .populate("carId")
       .populate("userId")
-      .sort({ _id: -1 });
-      
-    return res.json({ message: "success", ordersData });
+      .sort({ _id: -1 })
+      .skip(SKIP)
+      .limit(LIMIT);
+
+    return res.json({ message: "success", ordersData, size });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: "Internal Server Error" });
@@ -58,6 +73,23 @@ export const rejectOrder = async (req, res) => {
         },
       }
     );
+
+    const orderData = await ordersSchema.findOne({ _id: orderId });
+
+    const walletData = {
+      amount: orderData.totalPrice,
+      type: "credit",
+      description: "Rejected car rental refund",
+    };
+
+    const result = await walletSchema.updateOne(
+      { userId: orderData.userId },
+      {
+        $inc: { balance: orderData.totalPrice },
+        $push: { history: walletData },
+      }
+    );
+    
     return res.json({ message: "success" });
   } catch (error) {
     console.error(error);
