4e9c6994f587824968c162d012975307d9d88962
raheem-zk <muhammedraheem144@gmail.com>
2023-11-08 20:12:01 +0530

    Add admin dashboard function

--- backend/controller/admin/controller.js
+++ backend/controller/admin/controller.js
@@ -1,4 +1,7 @@
-import orderShema from '../../models/order.js';
+import orderShema from "../../models/order.js";
+import userShema from "../../models/user.js";
+import carOwnerShema from "../../models/carOwner/carOwner.js";
+import carShema from "../../models/carOwner/car.js";
 
 const LIMIT = 10;
 
@@ -12,15 +15,46 @@ export const getOrders = async (req, res) => {
 
     const SKIP = (PAGE - 1) * LIMIT;
 
-    const data = await orderShema.find().populate('carId').populate('userId')
-    .sort({_id:-1})
-    .skip(SKIP)
-    .limit(LIMIT) ?? [];
+    const data =
+      (await orderShema
+        .find()
+        .populate("carId")
+        .populate("userId")
+        .sort({ _id: -1 })
+        .skip(SKIP)
+        .limit(LIMIT)) ?? [];
 
     const TotalSize = await orderShema.countDocuments();
     const size = Math.ceil(TotalSize / LIMIT);
-    
-    return res.json({message:'success', ordersData: data, size})
+
+    return res.json({ message: "success", ordersData: data, size });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ message: "Internal Server Error", error: true });
+  }
+};
+
+export const dashboard = async (req, res) => {
+  try {
+    let statusData = {};
+    const orderData = await orderShema.find({ status: "finished" });
+
+    if (orderData.length === 0) {
+      statusData.totalRevenue = 0;
+    } else {
+      const total = orderData.reduce((sum, order) => {
+        return sum + (order?.totalPrice || 0);
+      }, 0);
+      statusData.totalRevenue = total;
+    }
+
+    statusData.totalUser = await userShema.countDocuments();
+    statusData.totalCarOwner = await carOwnerShema.countDocuments();
+    statusData.totalCar = await carShema.countDocuments();
+
+    return res.json({ message: "success", statusData });
   } catch (error) {
     console.error(error);
     return res
