cba920f466569a6b2af99e523a376adfcda57f90
raheem-zk <muhammedraheem144@gmail.com>
2023-11-10 13:50:18 +0530

    Implement Socket.IO for real-time communication

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,15 @@
 import "dotenv/config";
-import express, { json } from 'express';
+import express from 'express';
 import dbConnect from './config/config.js';
 import userRouter from "./routes/userRoutes.js";
 import adminRouter from './routes/adminRoutes.js';
 import carownerRouter from './routes/carOwnerRoutes.js';
 import chatRouter from './routes/chatRoutes.js';
 import messageRouter from './routes/messageRoutes.js';
+import http from 'http';
+import { Server } from 'socket.io';
 import cors from 'cors'
+import configureSocket from "./config/socket.js";
 const app = express();
 
 dbConnect();
@@ -15,12 +18,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const CLIENT_URL = process.env.CLIENT_URL
-
-app.use(cors({
+const corsOptions = {
     origin: CLIENT_URL,
     methods:'GET,POST,PUT,DELETE,PATCH',
     credentials:true
-}))
+}
+app.use(cors(corsOptions))
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors:CLIENT_URL,
+});
+  
+configureSocket(io)
 
 app.use(express.static('public'))
 
@@ -31,6 +41,6 @@ app.use('/chat', chatRouter);
 app.use('/message', messageRouter)
 
 const PORT = process.env.PORT ?? 4000;
-app.listen(PORT ,()=>{
+server.listen(PORT ,()=>{
     console.log(`Example app listening on port ${PORT}`);
 })
--- frontend/src/component/chat/chat.tsx
+++ frontend/src/component/chat/chat.tsx
@@ -3,6 +3,10 @@ import { getChatsAPI } from "../../api/chatApi";
 import { useSelector } from "react-redux";
 import Conversation from "./conversation";
 import ChatBox from "./chatBox";
+import { io } from "socket.io-client";
+
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+const socket = io(BACKEND_URL);
 
 const Chat = ({ role }) => {
   const { carOwner } = useSelector((state: any) => state.carOwnerAuth);
@@ -10,6 +14,10 @@ const Chat = ({ role }) => {
   const [chats, setChats] = useState([]);
   const [currentChat, setCurrentChat] = useState(null);
   const [currentUserId, setCurrentUserId] = useState("");
+  const [onlineUsers, setOnlineUsers] = useState([]);
+  const [sendMessage, setSendMessage] = useState(null);
+  const [recieveMessage, setRecieveMessage] = useState(null);
+
   const getChats = async (id) => {
     const data = await getChatsAPI(id);
     setChats(data);
@@ -24,6 +32,41 @@ const Chat = ({ role }) => {
       setCurrentUserId(carOwner._id);
     }
   }, []);
+  
+  const handleGetUsers = (users) => {
+    setOnlineUsers(users);
+  };
+
+  useEffect(() => {
+    if (currentUserId !== "") {
+      socket.emit("new-user-add", currentUserId);
+      socket.on("get-users", handleGetUsers);
+    }
+
+    return () => {
+      socket.off("get-users", handleGetUsers);
+    };
+  }, [currentUserId]);
+
+  useEffect(() => {
+    if (sendMessage !== null) {
+      socket.emit("send-message", sendMessage);
+    }
+  }, [sendMessage]);
+  useEffect(() => {
+    socket.on("receive-message", (data) => {
+      setRecieveMessage(data);
+    });
+  }, []);
+
+  const checkOnlineStatus = (chat) => {
+    const chatMember =
+      chat?.userId?._id === currentUserId
+        ? chat?.ownerId?._id
+        : chat?.userId?._id;
+    const online = onlineUsers.find((user) => user?.userId === chatMember);
+    return !!online;
+  };
 
   return (
     <div className="Chat w-full flex bg-gray-50">
@@ -42,7 +85,11 @@ const Chat = ({ role }) => {
             ) : (
               chats.map((chat) => (
                 <div key={chat?._id} onClick={() => setCurrentChat(chat)}>
-                  <Conversation role={role} data={chat} />
+                  <Conversation
+                    role={role}
+                    data={chat}
+                    online={checkOnlineStatus(chat)}
+                  />
                 </div>
               ))
             )}
@@ -51,7 +98,13 @@ const Chat = ({ role }) => {
       </div>
 
       <div className="Right-side-chat w-4/6 bg-white">
-        <ChatBox role={role} chat={currentChat} currentUserId={currentUserId} />
+        <ChatBox
+          recieveMessage={recieveMessage}
+          role={role}
+          chat={currentChat}
+          currentUserId={currentUserId}
+          setSendMessage={setSendMessage}
+        />
       </div>
     </div>
   );
