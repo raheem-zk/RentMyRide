9ccf2ea600bba84fd999aed87bd04237cdb20d76
raheem-zk <muhammedraheem144@gmail.com>
2023-11-11 00:05:55 +0530

    Update Payment Fail Component Style

--- frontend/src/component/payment/paymentFail.tsx
+++ frontend/src/component/payment/paymentFail.tsx
@@ -1,17 +1,25 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
+const img =
+  "https://cdn.dribbble.com/users/34292/screenshots/5371308/expiredfailedpaymentvdrib.gif";
 const PaymentFail = () => {
-
   return (
-    <div className="min-h-screen flex items-center justify-center bg-red-200">
-      <div className="bg-white p-8 rounded-lg shadow-lg">
-        <h2 className="text-2xl font-bold mb-4">Payment Failed</h2>
-        <p>Sorry, your payment could not be processed.</p>
-        <p>Please check your payment details and try again.</p>
-        <p>If the problem persists, contact our support team for assistance.</p>
-      </div>
+    <div className="flex items-center justify-center min-h-screen bg-red-100">
+      {" "}
+      <img
+        src={img}
+        alt="payment failed image"
+        className="md:p-10 w-1/2 h-auto rounded-lg shadow-lg mb-4"
+      />{" "}
+      <Link to={"/"}>
+        {" "}
+        <button className="mx-5 px-6 py-3 bg-blue-500 text-white rounded-full hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue">
+          {" "}
+          Back to Home{" "}
+        </button>{" "}
+      </Link>{" "}
     </div>
-  )
-}
+  );
+};
 
-export default PaymentFail
+export default PaymentFail;
