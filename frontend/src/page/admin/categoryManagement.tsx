b9eb31a8fc991fd316daf60b48482471ab6c6fbb
raheem-zk <muhammedraheem144@gmail.com>
2023-11-15 21:14:00 +0530

    Added category management component

---
+++ frontend/src/page/admin/categoryManagement.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import CategoryManagement from "../../component/adminComponent/resources/categoryManagement";
+
+const Category = () => {
+  return <CategoryManagement/>
+};
+
+export default Category;
