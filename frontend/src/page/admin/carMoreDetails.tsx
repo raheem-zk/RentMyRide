6797c2f93eea47b8957339842bb3e9198b1ef6f6
raheem-zk <muhammedraheem144@gmail.com>
2023-11-17 10:48:31 +0530

    Handled 'Car More Data' side data not found error for improved user experience

--- frontend/src/page/admin/carMoreDetails.tsx
+++ frontend/src/page/admin/carMoreDetails.tsx
@@ -1,14 +1,33 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 import MoreDetails from "../../component/adminComponent/carMoreDetails";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
+import Loading from "../../component/loading";
 
 const CarMoreDetails = () => {
+  const navigate = useNavigate();
   const { carId } = useParams();
   const { cars } = useSelector((state: any) => state.carsList);
-  const car = cars.find((car) => car._id === carId);
+  const [car, setCar] = useState({});
+  const [load, setLoad] = useState(true);
+  console.log(cars, car)
+  useEffect(() => {
+    if (!cars || cars.length == 0) {
+      navigate("/admin/cars");
+    } else {
+      setLoad(false);
+      const car = cars.find((car) => car._id === carId);
+      setCar(car);
+    }
+  }, [cars]);
 
-  return <MoreDetails car={car} />;
+  useEffect(() => {
+    if (!car) {
+      navigate("/admin/cars");
+    }
+  }, [car]);
+
+  return load ? <Loading /> : <MoreDetails car={car} />;
 };
 
 export default CarMoreDetails;
